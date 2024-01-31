65389dd6cf261a16cfb1a3eef174f18c73956836
Tyler <khuuthah1001@gmail.com>
2024-01-31 14:32:24 +0900

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Go Highlighting
+vim.g["go_highlight_types"] = 1
+vim.g["go_highlight_fields"] = 1
+vim.g["go_highlight_functions"] = 1
+vim.g["go_highlight_function_calls"] = 1
+vim.g["go_highlight_operators"] = 1
+vim.g["go_highlight_extra_types"] = 1
+vim.g["go_highlight_build_contrainst"] = 1
