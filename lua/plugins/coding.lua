b8e3a2d3f5c232cdb5d858008e234b900f9cfe10
Tyler <khuuthah1001@gmail.com>
2024-11-13 14:03:31 +0900

    add more formatter

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -16,7 +16,16 @@ return {
       optional = true,
       opts = {
         formatters_by_ft = {
+          lua = { "stylua" },
           go = { "goimports", "gofumpt" },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+        format_on_save = {
+          lsp_format = "fallback",
+          timeout_ms = 500,
+        },
+        format_after_save = {
+          lsp_format = "fallback",
         },
       },
     },
